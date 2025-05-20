361890566294c20600000eba85b66b05d8d48d76
alex-termux <aleksejkosarevskij@gmail.com>
2025-05-21 00:07:40 +0200

    Add Makefile, scripts, src

---
+++ src/conveyor.h
@@ -0,0 +1,9 @@
+#ifndef CONVEYOR_H
+#define CONVEYOR_H
+
+typedef struct Conveyor {
+    int x;
+    int y;
+} Conveyor;
+
+#endif
---
+++ src/enums.h
@@ -0,0 +1,11 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum {
+    DIR_NORTH,
+    DIR_WEST,
+    DIR_SOUTH,
+    DIR_EAST
+};
+
+#endif
